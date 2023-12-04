31f4cfb80d0b1fd623156ddd9f6fe2c626be9dfc
Pasindu Peiris <it21831454@my.sliit.lk>
2023-12-05 00:03:35 +0530

    Update

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+const Front = document.getElementById('Front');
+const cards = document.querySelectorAll('.card');
+
+Front.addEventListener('mousemove', function(){
+
+  cards[0].style.rote
+
+
+})
